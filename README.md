95fbffabd7d45cbf2b3770ff99da853e899936fb
Cherry <mcherry@qti.qualcomm.com>
2016-08-15 14:11:00 -0700

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # git_demo
 Demo repo to learn about GIT
+
+Version 1 - 8-15-2016
---
+++ main.h
@@ -0,0 +1,16 @@
+/**
+ * @file main.h
+ * @brief demo text to learn git
+ * @author Mark Cherry, Qualcomm
+ *
+ * Various GIT Stuff
+ **/
+ 
+ #include <stdio.h>
+ #include "adc.h"
+ 
+ #define	SR_48000	48000
+ #define 	SR_44100	44100
+ #define	PER_48000	1.0/48000.0
+ #define	PER_44100	1.0/44100.0
+ 
