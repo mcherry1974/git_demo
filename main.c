522aa28ababca79be84d5a4c9881759168e53644
Cherry <mcherry@qti.qualcomm.com>
2016-08-15 15:22:54 -0700

    Second commit

---
+++ main.c
@@ -0,0 +1,17 @@
+/**
+ * @file main.c
+ * @brief demo text to learn git
+ * @author Mark Cherry, Qualcomm
+ *
+ * Various GIT Stuff
+ **/
+ 
+ #include <stdio.h>
+ #include "main.h"
+ 
+ void main(void){
+	
+	printf("GIT Test");
+ 
+ }
+ 
--- main.h
+++ main.h
@@ -6,9 +6,7 @@
  * Various GIT Stuff
  **/
  
- #include <stdio.h>
- #include "adc.h"
- 
+
  #define	SR_48000	48000
  #define 	SR_44100	44100
  #define	PER_48000	1.0/48000.0
